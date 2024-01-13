6feb0bb51e29b80ecd4fe8b6a984b7609c643513
stephadillo <shcedillo@gmail.com>
2024-01-13 13:24:59 -0600

    first ggplot attempted with mpg data frame

--- r4ds.R
+++ r4ds.R
@@ -1,1 +1,10 @@
-#r4ds
+# r4ds - Chapter 1
+
+# ggplot2 - implements the grammar of graphics ie it describes and builds graphs
+
+# data frame - rectangular collection of variables (in cols) and obs (in rows)
+
+# plot shows a negative relationship between engine size (displ) and fuel efficiency (hwy)
+# ie cars with big engines use more fuel
+ggplot(data = mpg) + geom_point(mapping = aes(x = displ, y = hwy))
+
